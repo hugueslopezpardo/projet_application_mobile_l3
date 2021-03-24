d95a8fbdfa9977a6a56512277b12b60c017e6bd4
= <=>
2021-03-25 00:52:57 +0100

    Début du fonctionnement de la todolist

--- src/store/accounts/actions.js
+++ src/store/accounts/actions.js
@@ -1,3 +1,5 @@
+import router from "@/router";
+
 const axios = require('axios');
 
 
@@ -19,17 +21,36 @@ export function API_REQUEST_SIGNUP({commit}, {name, email, password})
             email    : email,
             password : password
         })
-        .then(response => (
+        .then((response) => {
 
-            //commit('SET_LOGIN_INFORMATION', response['data'].token)
-            commit('SET_SUCCESS_MESSAGE', 'Inscription réussi !'),
-            console.log(response)
+            commit('SET_LOGIN_INFORMATION', response['data'].token),
+            commit('SET_SUCCESS_MESSAGE', 'Inscription réussi, redirection en cours !'),
 
-        )).catch((
+            setTimeout(() => {
+                router.push('/home')
+            }, 1000)
 
-            commit('SET_ERROR_MESSAGE', 'Inscription échoué !')
+        }).catch((error) => {
 
-        )).finally(() => commit('SET_IS_DATA_LOADING_FALSE')) //On arrête le chargement
+            if (error.response) {
+
+                if(error.response.status == 409) {  //Le compte est déja enregistré
+
+                    commit('SET_ERROR_MESSAGE', 'Compte déja enregistré')
+
+                }else if(error.request) {
+
+                    commit('SET_ERROR_MESSAGE', 'Service indisponible')
+
+                }else{
+
+                    commit('SET_ERROR_MESSAGE', "Une erreur innatendu c'est produite")
+
+                }
+
+            }
+
+        }).finally(() => commit('SET_IS_DATA_LOADING_FALSE')) //On arrête le chargement
 
 }
 
@@ -39,8 +60,45 @@ export function API_REQUEST_SIGNUP({commit}, {name, email, password})
  * Permet de se connecter
  * @axios : POST
  */
-export function API_REQUEST_LOGIN()
+export function API_REQUEST_LOGIN({commit},{email, password})
 {
+
+    commit('SET_IS_DATA_LOADING_TRUE')  //On lance le chargement
+
+    axios
+        .post('http://138.68.74.39/api/login', {
+
+            email    : email,
+            password : password
+
+        })
+        .then(response => (
+
+            commit('SET_LOGIN_INFORMATION', response['data'].token),
+            commit('SET_SUCCESS_MESSAGE', 'Connexion bien réussi, redirection en cours !'),
+
+            setTimeout(() => {
+                router.push('/home')
+            }, 1000)
+
+        ))
+        .catch(error => (
+
+            commit('SET_ERROR_MESSAGE', 'Connexion échoué !'),
+            console.log(error)
+
+        ))
+        .finally(() => commit('SET_IS_DATA_LOADING_FALSE')) //On arrête le chargement
+}
+
+/**
+ * Demande pour que les message soit reset pour éviter que il reste afficher
+ * @param commit
+ * @constructor
+ */
+export function RESET_MESSAGE({commit})
+{
+    commit('SET_RESET_MESSAGES')
 
 }
 
