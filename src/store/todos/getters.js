927ef5b5499c53fc3f65a9f78021e06325688242
faina <faina@hugues.home>
2021-03-25 01:00:43 +0100

    Travaille sur le store pour pouvoir traiter les TODOS avec AXIOS

---
+++ src/store/todos/getters.js
@@ -0,0 +1,24 @@
+export function GET_IS_DATA_LOADING(state)
+{
+    return state.is_data_loading
+}
+
+export function GET_SUCCESS_MESSAGE(state)
+{
+    return state.success_message
+}
+
+export function GET_ERROR_MESSAGE(state)
+{
+    return state.error_message
+}
+
+export function GET_TODOS_LISTS(state)
+{
+    return state.todos_lists
+}
+
+export function GET_CURRENT_LIST(state)
+{
+    return state.current_list
+}
---
+++ src/store/todos/mutations.js
@@ -0,0 +1,29 @@
+export function SET_IS_DATA_LOADING_TRUE(state)
+{
+    state.is_data_loading = true
+}
+
+export function SET_IS_DATA_LOADING_FALSE(state)
+{
+    state.is_data_loading = false
+}
+
+export function SET_TODOS_LISTS(state, api_todos_lists)
+{
+    state.todos_lists = api_todos_lists
+}
+
+export function SET_SUCCESS_MESSAGE(state, success_message)
+{
+    state.success_message = success_message
+}
+
+export function SET_ERROR_MESSAGE(state, error_message)
+{
+    state.error_message = error_message
+}
+
+export function SET_CURRENT_LIST(state, list_position_id)
+{
+    state.current_list = state.todos_lists[list_position_id]
+}
